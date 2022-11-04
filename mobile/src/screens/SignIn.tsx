4d66af2ce9a7d69337a42d65b624dd4b1887036d
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-11-04 10:51:35 -0300

    component: Loading and SingIn

---
+++ mobile/src/screens/SignIn.tsx
@@ -0,0 +1,11 @@
+import { Center, Text } from "native-base";
+
+export function SignIn() {
+    return (
+        <Center flex={1} bgColor="black">
+            <Text color="white" fontSize={24}>
+                SingIn!
+            </Text>
+        </Center>
+    )
+}
