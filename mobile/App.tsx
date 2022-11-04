fd98a9d2e3216803af3d0854714e51a1e3251c7b
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-11-04 15:36:19 -0300

    configurando a StatusBar

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,5 @@
-import { NativeBaseProvider } from "native-base";
+import { NativeBaseProvider, StatusBar } from "native-base";
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
-
 
 import { Loading } from './src/components/Loading';
 import { SignIn } from './src/screens/Signin';
@@ -12,9 +11,13 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
-      {
-        fontLoaded ? <SignIn /> : <Loading />
-      }
+       <StatusBar
+          barStyle="light-content"
+          backgroundColor="trasparent"
+          translucent
+       />
+
+      { fontLoaded ? <SignIn /> : <Loading /> }
     </NativeBaseProvider>
   );
 }
