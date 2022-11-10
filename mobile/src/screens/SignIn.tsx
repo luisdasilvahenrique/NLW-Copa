798914766795632df22e35bf54cef63ce635b922
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-11-10 18:24:46 -0300

    Component: BUTTON

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,11 +1,20 @@
-import { Center, Text } from "native-base";
+import { Center, Text, Icon } from "native-base";
+import { Fontisto } from '@expo/vector-icons';
+
+import Logo from '../assets/logo.svg';
+import { Button } from "../components/Button";
+
 
 export function SignIn() {
     return (
         <Center flex={1} bgColor="black">
-            <Text color="white" fontSize={24}>
-                SingIn!
-            </Text>
+               <Logo width={212} height={40}/> 
+
+               <Button 
+               leftIcon={<Icon as={Fontisto} name="google" color="white" size="md"/>}
+               title='ENTRAR COM O GOOGLE'
+               type="SECONDARY"
+               />
         </Center>
     )
 }
